ce228a1feb4540659dfd2640a1b1514dbb606d51
Manfrinmm <matheus_mm123@hotmail.com>
2020-09-10 18:53:03 -0300

    chore: edit api route & style package

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -10,7 +10,7 @@ const Header = () => (
 
     <User>
       <img
-        src="https://scontent.fgyn1-1.fna.fbcdn.net/v/t1.0-1/p240x240/31841629_1534536563323875_1900707119368241152_n.jpg?_nc_cat=104&_nc_oc=AQkB5x2DLiIbIIiWHL2wfoU5T2i0-hOc0QCmnKZysxw38eaCERIxgpQ_l4r6-yuVAnE&_nc_pt=1&_nc_ht=scontent.fgyn1-1.fna&oh=2fd03da03d2e75234ce53af8d7e28c44&oe=5DE3F8E0"
+        src="https://media-exp1.licdn.com/dms/image/C4D03AQFT_RrYP1_BFg/profile-displayphoto-shrink_200_200/0?e=1602115200&v=beta&t=0Fcoa5IE7jaTHhIva9RazjPLH0HAdSKYdVfeRaJ_49I"
         alt="Avatar"
       />
       Matheus Menezes
